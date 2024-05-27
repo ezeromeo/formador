7268c525e1031135de357aba5a99f19d833a6a3e
Ezequiel Romeo <ezequiel.romeo@gmail.com>
2024-05-27 19:39:01 -0300

    finish

--- js/script.js
+++ js/script.js
@@ -32,4 +32,121 @@ document.addEventListener("DOMContentLoaded", function() {
     const activarChatbot = document.getElementById('activarChatbot');
     const chatbotModal = new bootstrap.Modal(document.getElementById('chatbotModal'));
     activarChatbot.addEventListener('click', ()=> chatbotModal.show());
+
+    const activarAgenda = document.getElementById('activarAgenda');
+    const agendaModal = new bootstrap.Modal(document.getElementById('agendaModal'));
+    activarAgenda.addEventListener('click', ()=> agendaModal.show());
+
+    const tbodyCalendar = document.getElementById('calendarBody');
+    let weekRow = '<tr>';
+    const daysArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+    daysArray.forEach((day, index) => {
+    
+        let isWeekday = (index % 7) < 5;
+        if (isWeekday) {
+            weekRow += `<td><div class="weekday">${day}</div></td>`;
+        } else {
+            weekRow += `<td><div class="weekend">${day}</div></td>`;
+        }
+        
+    
+        if ((index + 1) % 7 === 0 || index === daysArray.length - 1) {
+            weekRow += '</tr>';
+            tbodyCalendar.innerHTML += weekRow;
+            weekRow = '<tr>';
+        }
+    });
+
+    const messageBox = document.getElementById('messageBox');
+    const messagesContainer = document.getElementById('messagesContainer');
+    const fileInput = document.getElementById('fileInput');
+    const attachButton = document.getElementById('attachButton');
+    const cancelAttachmentButton = document.getElementById('cancelAttachmentButton');
+    const sendButton = document.getElementById('sendButton');
+
+    attachButton.addEventListener('click', function() {
+        fileInput.click();
+    });
+
+    cancelAttachmentButton.addEventListener('click', function() {
+        fileInput.value = '';
+    });
+
+    sendButton.addEventListener('click', function() {
+        const messageText = messageBox.value.trim();
+        
+        if (messageText || fileInput.files.length > 0) {
+            const messageElement = document.createElement('div');
+            messageElement.className = 'chat-message';
+    
+            const superiorElement = document.createElement('div');
+            superiorElement.className = 'superior d-flex align-items-center justify-content-between';
+            const nombreElement = document.createElement('p');
+            nombreElement.className = 'nombreSuperior ms-2';
+            nombreElement.textContent = 'Diego Pérez'; 
+            const fechaElement = document.createElement('p');
+            fechaElement.className = 'fechaSuperior me-2';
+            fechaElement.textContent = new Date().toLocaleString(); 
+    
+            superiorElement.appendChild(nombreElement);
+            superiorElement.appendChild(fechaElement);
+    
+            const inferiorElement = document.createElement('div');
+            inferiorElement.className = 'inferior';
+            const textoElement = document.createElement('p');
+            textoElement.className = 'textoInferior';
+            textoElement.textContent = messageText;
+    
+            inferiorElement.appendChild(textoElement);
+    
+            
+            if (fileInput.files.length > 0) {
+                const archivoElement = document.createElement('p');
+                archivoElement.className = 'textoInferior';
+                archivoElement.textContent = `Adjunto: ${fileInput.files[0].name}`;
+                inferiorElement.appendChild(archivoElement);
+            }
+    
+            messageElement.appendChild(superiorElement);
+            messageElement.appendChild(inferiorElement);
+    
+            messagesContainer.appendChild(messageElement);
+    
+            
+            messageBox.value = '';
+            fileInput.value = ''; 
+            if (fileDisplay) {
+                fileDisplay.value = ''; 
+            }
+        }
+    });
+    
+
+    const activarBell = document.getElementById('activarBell');
+    const chatModal = new bootstrap.Modal(document.getElementById('chatModal'));
+    activarBell.addEventListener('click', ()=> chatModal.show());
+
+    attachButton.addEventListener('click', function() {
+        fileInput.click(); 
+    });
+    
+    fileInput.addEventListener('change', function() {
+        if (fileInput.files.length > 0) {
+            fileDisplay.value = `Adjuntar: ${fileInput.files[0].name}`; 
+        }
+    });
+    
+    cancelAttachmentButton.addEventListener('click', function() {
+        fileInput.value = ''; 
+        fileDisplay.value = ''; 
+    });
+    
+    fileDisplay.addEventListener('click', function(event) {
+        event.stopPropagation(); 
+    });
+    
+
 });
+
+
